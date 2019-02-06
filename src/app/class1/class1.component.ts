a64b5a00eb1a702983cb523f5823ec21d33be8e0
ivy <original541029@gmail.com>
2019-02-06 21:37:46 +0800

    class1 layout

--- src/app/class1/class1.component.ts
+++ src/app/class1/class1.component.ts
@@ -6,32 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./class1.component.sass']
 })
 export class Class1Component implements OnInit {
-  newResult: object[]
-  pageTitle: string
-  pageEnTitle: string
+  newResult: object[];
+  pageTitle: string;
+  pageEnTitle: string;
 
   constructor() { }
 
   ngOnInit() {
-    this.newResult = this.getData()
-    this.pageTitle = '九九乘法表'
-    this.pageEnTitle = 'MULTIPLICATION CHART'
+    this.newResult = this.getData();
+    this.pageTitle = '九九乘法表';
+    this.pageEnTitle = 'MULTIPLICATION CHART';
   }
 
-  getData () {
-    const row = 9
-    let newResult = []
-    for (let i = 2 ; i<= row ; i++) {
-      let data = []
-      for (let g = 1 ; g <= row ; g++) {
-        data.push(i + ' x ' + g + ' = ' + i*g)
+  getData() {
+    const row = 9;
+    const newResult = [];
+    for (let i = 2; i <= row; i++) {
+      const data = [] ;
+      for (let g = 1; g <= row; g++) {
+        data.push(i + ' x ' + g + ' = ' + i * g) ;
       }
       newResult.push({
         title: i.toString(),
         'data': data
-      })
+      });
     }
-    console.log('newResult', newResult)
-    return newResult
+    console.log('newResult', newResult);
+    return newResult;
   }
 }
